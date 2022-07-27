41ba0fe0996970cdc1a46b98763b62056dbb8a86
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-07-27 09:34:09 +0200

    Delete Client Mutation & updating cache

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,12 +1,13 @@
 import { useQuery } from "@apollo/client"
 import { GET_CLIENTS } from "../queries/clientQueries"
 import ClientRow from "./ClientRow"
+import Spinner from "./Spinner"
 
 export default function Clients() {
   // Get clients
   const { loading, error, data } = useQuery(GET_CLIENTS)
 
-  if (loading) return <p>Loading...</p>
+  if (loading) return <Spinner />
   if (error) return <p>Something Went Wrong</p>
 
   return (
