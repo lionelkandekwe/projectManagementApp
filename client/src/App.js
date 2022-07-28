84a5da6c35c9da66d93de3cdcb1f27716738932a
Lionel Kandekwe <lionelkandekwe@Lionels-MacBook-Pro.local>
2022-07-28 09:29:03 +0200

    display project

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header"
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import Project from "./pages/Project"
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -38,6 +39,7 @@ function App() {
           <div className="container">
             <Routes>
               <Route path="/" element={<Home />} />
+              <Route path="/projects/:id" element={<Project />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
           </div>
